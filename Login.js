ab128f77e96e8f8143e9cae7b2c6671c10db67e3
musaibdeveloper <syedmusaib428@gmail.com>
2023-08-30 10:55:38 +0000

    DataBase is added

--- Login.js
+++ Login.js
@@ -1,9 +1,26 @@
 import readline from "readline-sync"
+import fs from 'fs/promises';
 
 export default async function Login(){
     let firstName = readline.question("Enter your name: ")
     let lastName  = readline.question("Enter the last name : ")
-    let password = readline.question("Enter Password:", { hideEchoBack: true });   
+    let password = readline.question("Enter Password:", { hideEchoBack: true }); 
+    
+    
+    let dataBase = {
+        firstName,
+        lastName,
+        password
+    }
+
+    let stores = await fs.readFile("dataBase.json", "utf-8"); // "[]"
+    let stringsTobj = JSON.parse(stores);
+    stringsTobj.push(dataBase);
+    let objstostr = JSON.stringify(stringsTobj);
+    await fs.writeFile("dataBase.json" , objstostr );
+
+
+
 
    
 }
--- Register.js
+++ Register.js
@@ -1,4 +1,6 @@
 import readline from "readline-sync"
+import fs from "fs/promises"
+
 
 export default async function Register(){
     let firstName = readline.question("Enter your name: ")
@@ -6,6 +8,24 @@ export default async function Register(){
     let phonenumber = +readline.question("Enter your phone number: ")
     let password = readline.question("Enter  new Password: ", { hideEchoBack: true }); 
     let Confirmpassword = readline.question("Re-Enter  new Password: ", { hideEchoBack: true });   
-  
-}
 
+
+    let dataBase = {
+        firstName,
+        lastName,
+        phonenumber,
+        password,
+        Confirmpassword
+    }
+    
+     let store = await fs.readFile("dataBase.json", "utf-8"); // "[]"
+    
+     let stringTobj = JSON.parse(store); // []
+     stringTobj.push(dataBase);         // [push]
+     let objtostr = JSON.stringify(stringTobj);  // ["data"]
+    //  console.log(objtostr);
+     await fs.writeFile("dataBase.json", objtostr) // write in the file.
+    
+
+
+}
